566d3fc8dd59d4366ea786df4c6f013121e5f48b
akshay <akshay@deliforce.io>
2021-02-02 13:18:55 +0530

    Feature: Added Redis adapter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { RedisIoAdapter } from './adapters/redis.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:4200', // angular
-    ],
-  });
-  await app.listen(4000,'0.0.0.0');
+  app.enableCors();
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
+  await app.listen(4000, '0.0.0.0');
 }
 bootstrap();
